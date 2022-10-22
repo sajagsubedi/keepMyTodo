1865fdda645c073dfa27521865e8f37bc1480725
Sajag Subedi <108583402+sajagsubedi03@users.noreply.github.com>
2022-10-22 15:39:28 +0545

    update script.js

--- script.js
+++ script.js
@@ -17,11 +17,15 @@ localStorage.setItem('notes',JSON.stringify(notesObj))
 }
 tit.value=("")
 desc.value=("")
-alertbx.classList.add("bg-green-400")
+tit.classList.add("border-green-500")
+desc.classList.add("border-green-500")
+alertbx.classList.add("bg-green-500")
 alertbx.innerHTML=`<p class="text-white">Successfully added new Todo</p>`
 setTimeout(()=>{
-alertbx.classList.remove("bg-green-400")
+alertbx.classList.remove("bg-green-500")
 alertbx.innerHTML=``
+tit.classList.remove("border-green-500")
+desc.classList.remove("border-green-500")
 },1000)
 update();
 }
@@ -71,21 +75,19 @@ notesObj=JSON.parse(notes)
 let notesitems=" ";
 notesObj.map((element,index)=>{
 notesitems+=`
-  <div class="lg:flex my-3 min-h-[100px] min-w-[100px] mx-3 rounded lg:items-center w-1/1 bg-gray-800 lg:justify-between">
-  <div class="min-w-fit  min-h-[60px] flex-1">
-  <h2 class="text-2xl text-white font-bold leading-7  sm:truncate sm:text-3xl sm:tracking-tight"><span class="text-violet-500 mx-2">${index+1}</span>${element[0]}</h2>
-  <div class="mt-1 flex flex-col sm:mt-0 sm:flex-row sm:flex-wrap sm:space-x-6">
-  <div class="mt-2 flex items-center text-1xl text-gray-400">
-  ${element[1]}
-  </div> 
-  </div>
-  </div>
-  <span class="sm:ml-3 min-w-fit flex  items-center justify-center ">
-  <button onclick="deletei(${index})" type="button" class="inline-flex items-center rounded-md border border-transparent bg-blue-600 px-4 py-1 text-sm font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
-  Delete
-  </button>
-  </span>
-  </div>`
+<div class="lg:flex my-5 min-h-[100px] px-5 py-3 border border-gray-700 mx-3 rounded lg:items-center w-1/1 bg-gray-800 lg:justify-between">
+<div class="min-w-fit  min-h-[60px] flex-1">
+<h2 class="text-2xl text-white font-bold leading-7  sm:truncate sm:text-3xl sm:tracking-tight">${element[0]}</h2>
+<div class="mt-1 flex flex-col sm:mt-0 sm:flex-row sm:flex-wrap sm:space-x-6">
+<div class="mt-2 flex items-center text-1xl text-gray-500">${element[1]}</div> 
+</div>
+</div>
+<span class="sm:ml-3 min-w-fit flex  items-center justify-center ">
+<button  onclick="deletei(${index})" type="button" class="inline-flex items-center rounded-md border border-transparent bg-red-600 px-4 py-1 text-sm font-medium text-white shadow-sm hover:bg-red-500 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
+Delete
+</button>
+</span>
+</div>`
   document.getElementById('notesdiv').innerHTML=notesitems
 })
 }
